32e66332364e5bf103c84e06b2434c74c186f2a7
kangjoseph90 <kangjoseph90@gmail.com>
2021-11-08 11:29:27 +0900

    solved

--- BOJ/1448.cpp
+++ BOJ/1448.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int n;
 
+int ans=-1;
+
 vector<int> v;
 
 
@@ -16,5 +18,11 @@ int main(){
         v.push_back(t);
     }
     sort(v.begin(),v.end());
+    for(int i=2;i<n;i++){
+        if(v[i-2]+v[i-1]>v[i]){
+            ans=max(ans,v[i-2]+v[i-1]+v[i]);
+        }
+    }
+    cout<<ans<<'\n';
 
 }
